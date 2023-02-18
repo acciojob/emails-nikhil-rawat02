84210345f08c1db39306786488537198f0b95a9f
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-02-18 12:44:05 +0530

    1 Solution updated

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -10,6 +10,10 @@ public class Email {
         this.password = "Accio@123";
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getEmailId() {
         return emailId;
     }
@@ -20,10 +24,36 @@ public class Email {
 
     public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        if(!oldPassword.equals(getPassword())){
+//            System.out.println("old password not match with current password! please try again");
+            return;
+        }
+
         // 1. It contains at least 8 characters
+        else if (newPassword.length() < 8 ){
+//            System.out.println(" Password length must be at least 8! please try again");
+            return;
+        }
         // 2. It contains at least one uppercase letter
+        boolean isUpperCase = false;
         // 3. It contains at least one lowercase letter
+        boolean isLowerCase = false;
         // 4. It contains at least one digit
+        boolean isDigit = false;
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        boolean isSpecialCharacter = false;
+        for(int i =0; i < newPassword.length(); i++){
+            char ch = newPassword.charAt(i);
+            if(ch >= 'A' && ch <= 'Z')isUpperCase = true;
+            if(ch >= 'a' && ch <= 'z')isLowerCase = true;
+            if(ch >= '0' && ch <= '9')isDigit = true;
+            if((ch < 'A' || ch > 'Z') &&(ch < '0' || ch > '9') && (ch < 'a' || ch > 'z') )isSpecialCharacter = true;
+        }
+        if(isUpperCase && isLowerCase && isDigit && isSpecialCharacter){
+            setPassword(newPassword);
+//            System.out.println("new Password has bee updated");
+        }else{
+//            System.out.println("new Password must contains at least one uppercase letter and at least one lowercase letter and at least one digit letter and at least one special character letter");
+        }
     }
 }
